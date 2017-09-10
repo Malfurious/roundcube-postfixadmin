0d54a688ac2eeb1345bb4bf19cd52a5d24640dcf
Malfurious <jmay9990@gmail.com>
2017-09-10 15:28:47 -0500

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,41 @@
+FROM malfurious/nginx-php
+
+LABEL description "Roundcube-Postfix is a simple, modern & fast webmail client combined with an administrative Postfixadmin webportal to manage postfix accounts." \
+      maintainer="Malfurious <jmay9990@gmail.com>"
+	  
+ARG ROUND_VERSION=1.3.1
+ARG POST_VERSION=3.1
+ENV UID=991 GID=991 UPLOAD_MAX_SIZE=25M MEMORY_LIMIT=128M
+RUN echo "@community https://nl.alpinelinux.org/alpine/v3.6/community" >> /etc/apk/repositories \
+ && apk -U upgrade
+RUN apk add gnupg openssl dovecot tini@community
+RUN apk add -t build-dependencies \
+    wget \
+    git \
+    curl \
+	ca-certificates \
+	coreutils \
+	gcc \
+	linux-headers \
+	make \
+	musl-dev \
+ && cd /tmp \
+ && wget -q https://github.com/roundcube/roundcubemail/releases/download/${ROUND_VERSION}/roundcubemail-${ROUND_VERSION}-complete.tar.gz \
+ && wget -q https://downloads.sourceforge.net/project/postfixadmin/postfixadmin/postfixadmin-${POST_VERSION}/postfixadmin-${POST_VERSION}.tar.gz
+RUN mkdir /roundcube && tar -xzf /tmp/roundcubemail-${ROUND_VERSION}-complete.tar.gz --strip 1 -C /roundcube \
+ && mv /roundcube/config/config.inc.php.sample /roundcube/config/config.inc.php && mv /roundcube/composer.json-dist /roundcube/composer.json
+RUN mkdir /postfixadmin && tar xzf /tmp/postfixadmin-${POST_VERSION}.tar.gz -C /postfixadmin && mv /postfixadmin/postfixadmin-${POST_VERSION}/* /postfixadmin
+RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/roundcube/ --filename=composer.phar
+RUN cd /roundcube && php composer.phar install --no-dev \
+ && find /roundcube -type d -exec chmod 755 {} \; \
+ && find /roundcube -type f -exec chmod 644 {} \; \
+ && apk del build-dependencies \
+ && rm -rf /tmp/* /var/cache/apk/* /root/.gnupg /postfixadmin/postfixadmin-${POST_VERSION}*
+
+RUN mkdir /enigma && mv /roundcube/plugins/password/config.inc.php.dist /roundcube/plugins/password/config.inc.php \
+ && mv /roundcube/plugins/enigma/config.inc.php.dist /roundcube/plugins/enigma/config.inc.php
+COPY rootfs /
+RUN chmod +x /usr/local/bin/* /etc/s6.d/*/* /etc/s6.d/.s6-svscan/*
+EXPOSE 8888 8080
+VOLUME /enigma
+CMD ["tini", "--", "run.sh"]
---
+++ rootfs/usr/local/bin/run.sh
@@ -0,0 +1,199 @@
+#!/bin/sh
+
+# Set attachment size and memory limit
+sed -i -e "s/<UPLOAD_MAX_SIZE>/$UPLOAD_MAX_SIZE/g" /nginx/conf/nginx.conf /php/etc/php-fpm.conf \
+       -e "s/<MEMORY_LIMIT>/$MEMORY_LIMIT/g" /php/etc/php-fpm.conf
+	   
+# Check Required Variables.
+if [ -z ${ROUND_USER} ]; then
+        echo "Roundcube MySQL Username not set!"
+        exit 1
+fi
+if [ -z ${ROUND_PASS} ]; then
+        echo "Roundcube MySQL Password not set!"
+        exit 1
+fi
+if [ -z ${ROUND_DB} ]; then
+        echo "Roundcube MySQL Database Name not set!"
+        exit 1
+fi
+if [ -z ${MAIL_HOST} ]; then
+        echo "Mail Hostname not set!"
+        exit 1
+fi
+if [ -z ${MYSQL_HOST} ]; then
+        echo "MySQL Hostname not set!"
+        exit 1
+fi
+if [ -z ${POST_USER} ]; then
+        echo "Postfixadmin MySQL Username not set!"
+        exit 1
+fi
+if [ -z ${POST_PASS} ]; then
+        echo "Postfixadmin MySQL Password not set!"
+        exit 1
+fi
+if [ -z ${POST_DB} ]; then
+        echo "Postfixadmin MySQL Database not set!"
+        exit 1
+fi
+
+DOMAIN=${MAIL_HOST#*.}
+DBHOST=${MYSQL_HOST}
+DBUSER=${POST_USER}
+DBNAME=${POST_DB}
+DBPASS=${POST_PASS}
+SMTPHOST=${MAIL_HOST}
+DISABLE_INSTALLER=${DISABLE_INSTALLER:-false}
+ENABLE_IMAPS=${ENABLE_IMAPS:-true}
+ENABLE_SMTPS=${ENABLE_SMTPS:-true}
+
+# Create smarty cache folder
+mkdir -p /postfixadmin/templates_c
+
+# Configure Encryption Method
+if [ -z ${PASS_CRYPT} ]; then
+		echo "No Password encryption set, using SHA512-CRYPT by default."
+		export PASS_CRYPT_LC="sha512-crypt"
+		export PASS_CRYPT_UC="SHA512-CRYPT"
+		export ENCRYPTION="dovecot:SHA512-CRYPT"
+else
+		echo "Using password encryption: ${PASS_CRYPT} "
+		export PASS_CRYPT_LC="sha512-crypt"
+		export PASS_CRYPT_UC=$(echo $PASS_CRYPT_LC | awk '{print toupper($0)}')
+		export ENCRYPTION="dovecot:$PASS_CRYPT_UC"
+fi
+
+# Enabling or Disabling Installer depending on variable.
+if [ ${DISABLE_INSTALLER} == "true" ]; then
+        export INST="\$config['enable_installer'] = false;"
+        sed -i "/roundcubemail';/a $INST" /roundcube/config/config.inc.php
+        echo "Disabling Installer."
+elif [ ${DISABLE_INSTALLER} == "false" ]; then
+        export INST="\$config['enable_installer'] = true;"
+        sed -i "/roundcubemail';/a $INST" /roundcube/config/config.inc.php
+        echo "Enabling Installer."
+fi
+
+# Configure MIME Types
+echo "Configuring MIME Types..."
+export MIME="\$config['mime_types'] = '/nginx/conf/mime.types';"
+sed -i "/roundcubemail';/a $MIME" /roundcube/config/config.inc.php
+echo "MIME Types Configured!"
+
+# Configure MySQL Connection.
+echo "Configuring MySQL Connection..."
+export SQL="\$config['db_dsnw'] = 'mysql://${MYSQL_USER}:${MYSQL_PASS}@${MYSQL_HOST}/${MYSQL_DATABASE}';"
+sed -i "/roundcubemail';/c $SQL" /roundcube/config/config.inc.php
+echo "MySQL Connection Configured!"
+
+# Configure Mailserver Connection.
+echo "Configuring Mailserver Connection."
+if [ ${ENABLE_IMAPS} == "true" ]; then
+        export IMAPS="\$config['default_host'] = 'imaps://${MAIL_HOST}';"
+        sed -i "/\$config\['default_host'\]/c $IMAPS" /roundcube/config/config.inc.php
+        export PRT="\$config['default_port'] = 993;"
+        sed -i "/imaps:\/\/${MAIL_HOST}/a $PRT" /roundcube/config/config.inc.php
+        echo "IMAPS Enabled, and Server Set!"
+elif [ ${ENABLE_IMAPS} == "false" ]; then
+        export IMAP="\$config['default_host'] = '${MAIL_HOST}';"
+        sed -i "/\$config\['default_host'\]/c $IMAP" /roundcube/config/config.inc.php
+        export PRT="\$config['default_port'] = 143;"
+        sed -i "/ssl:\/\/${MAIL_HOST}/a $PRT" /roundcube/config/config.inc.php
+        echo "IMAPS Disabled, and Server Set!"
+fi
+if [ ${ENABLE_SMTPS} == "true" ]; then
+        export SMTPS="\$config['smtp_server'] = 'tls://${MAIL_HOST}';"
+        export SMTPSPRT="\$config['smtp_port'] = 587;"
+        sed -i "/\$config\['smtp_server'\]/c $SMTPS" /roundcube/config/config.inc.php
+        sed -i "/\$config\['smtp_port'\]/c $SMTPSPRT" /roundcube/config/config.inc.php
+        echo "SMTPS Enabled, and Server Set!"
+elif [ ${ENABLE_SMTPS} == "false" ]; then
+        export SMTP="\$config['smtp_server'] = '${MAIL_HOST}';"
+        sed -i "/\$config\['smtp_server'\]/c $SMTP" /roundcube/config/config.inc.php
+        echo "SMTPS Disabled, and Server Set!"
+fi
+
+# Configure Plugins
+echo "Enabling Plugins..."
+export PLUGINS=" 'password','enigma'"
+sed -i "/'zipdownload',/a $PLUGINS" /roundcube/config/config.inc.php
+echo "Plugins: Password, and Enigma Enabled!!"
+
+# Configure Password/Postfixadmin plugin
+export 
+echo "Configuring password plugin with Postfixadmin information."
+export POST="\$config['password_db_dsn'] = 'mysql://${POST_USER}:${POST_PASS}@${MYSQL_HOST}/${POST_DB}';"
+export POSTCMD="\$config['password_query'] = 'UPDATE mailbox SET password=%P WHERE username=%u LIMIT 1';"
+export PASSALG="\$config['password_algorithm'] = '${PASS_CRYPT_LC}';"
+export PASSPRE="\$config['password_algorithm_prefix'] = '{${PASS_CRYPT_UC}}';"
+sed -i "/\$config\['password_db_dsn'\]/c $POST" /roundcube/plugins/password/config.inc.php
+sed -i "/\$config\['password_query'\]/c $POSTCMD" /roundcube/plugins/password/config.inc.php
+sed -i "/\$config\['password_algorithm'\]/c $PASSALG" /roundcube/plugins/password/config.inc.php
+sed -i "/\$config\['password_algorithm_prefix'\]/c $PASSPRE" /roundcube/plugins/password/config.inc.php
+echo "Password plugin Configured."
+
+# Configure Enigma GPG
+export GPGHOME="\$config['enigma_pgp_homedir'] = '/enigma';"
+sed -i "/\$config\['enigma_pgp_homedir'\]/c $GPGHOME" /roundcube/plugins/enigma/config.inc.php
+
+# Fix permissions
+chown -R $UID:$GID /roundcube /etc/s6.d /nginx /php /var/log /enigma /postfixadmin
+
+# Configure Listening Ports if changed.
+if [ -z ${POSTFIX_PORT} ]; then
+	echo "Postfix WebGUI Port not Specified, using default of 8080."
+else
+	echo "Postfix WebGUI Port Specified as ${POSTFIX_PORT}, configuring as such."
+	sed -i "s/8080/${POSTFIX_PORT}/g" /mnt/user/DockerData/tmp/rootfs/nginx/sites-enabled/postfixadmin.conf
+	echo "Postfix WebGUI Port configured!"
+fi
+if [ -z ${ROUNDCUBE_PORT} ]; then
+	echo "Roundcube WebGUI Port not Specified, using default of 8888."
+else
+	echo "Roundcube WebGUI Port Specified as ${ROUNDCUBE_PORT}, configuring as such."
+	sed -i "s/8888/${ROUNDCUBE_PORT}/g" /mnt/user/DockerData/tmp/rootfs/nginx/sites-enabled/roundcube.conf
+	echo "Roundcube WebGUI Port configured!"
+fi
+
+# Local postfixadmin configuration file
+cat > /postfixadmin/config.local.php <<EOF
+<?php
+\$CONF['configured'] = true;
+\$CONF['database_type'] = 'mysqli';
+\$CONF['database_host'] = '${DBHOST}';
+\$CONF['database_user'] = '${DBUSER}';
+\$CONF['database_password'] = '${DBPASS}';
+\$CONF['database_name'] = '${DBNAME}';
+\$CONF['encrypt'] = '${ENCRYPTION}';
+\$CONF['dovecotpw'] = "/usr/bin/doveadm pw";
+\$CONF['smtp_server'] = '${SMTPHOST}';
+\$CONF['domain_path'] = 'YES';
+\$CONF['domain_in_mailbox'] = 'NO';
+\$CONF['fetchmail'] = 'YES';
+\$CONF['sendmail'] = 'YES';
+\$CONF['admin_email'] = 'postfixadmin@${DOMAIN}';
+\$CONF['footer_text'] = 'Return to ${DOMAIN}';
+\$CONF['footer_link'] = 'http://${DOMAIN}';
+\$CONF['default_aliases'] = array (
+  'abuse'      => 'abuse@${DOMAIN}',
+  'hostmaster' => 'hostmaster@${DOMAIN}',
+  'postmaster' => 'postmaster@${DOMAIN}',
+  'webmaster'  => 'webmaster@${DOMAIN}'
+);
+\$CONF['quota'] = 'YES';
+\$CONF['domain_quota'] = 'YES';
+\$CONF['quota_multiplier'] = '1024000';
+\$CONF['used_quotas'] = 'YES';
+\$CONF['new_quota_table'] = 'YES';
+\$CONF['aliases'] = '0';
+\$CONF['mailboxes'] = '0';
+\$CONF['maxquota'] = '0';
+\$CONF['domain_quota_default'] = '0';
+?>
+EOF
+
+echo "Server is now started."
+echo "Roundcube is listening on port: ${ROUNDCUBE_PORT} & Postfixadmin is listening on port: ${POSTFIX_PORT}"
+# RUN !
+exec su-exec $UID:$GID /bin/s6-svscan /etc/s6.d
